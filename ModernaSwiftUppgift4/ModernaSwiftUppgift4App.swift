978de9344146d0b568177fe360a65f96c878fff4
Andreas Nef <andreas@ontherecord.se>
2024-11-07 23:17:41 +0100

    Initial Commit

---
+++ ModernaSwiftUppgift4/ModernaSwiftUppgift4App.swift
@@ -0,0 +1,32 @@
+//
+//  ModernaSwiftUppgift4App.swift
+//  ModernaSwiftUppgift4
+//
+//  Created by Andreas Nef on 07.11.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ModernaSwiftUppgift4App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
